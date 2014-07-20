881f0a7de20d66592373217c05a80db5316a529b
Tim Schumacher <tim@bandenkrieg.hacked.jp>
2014-07-20 09:49:01 +0200

    Completed the repeating event model.

--- src/Hackspace/Bundle/CalciferBundle/Entity/RepeatEvent.php
+++ src/Hackspace/Bundle/CalciferBundle/Entity/RepeatEvent.php
@@ -11,13 +11,67 @@ use Doctrine\ORM\PersistentCollection;
  * @ORM\Table(name="repeat_events")
  * @ORM\Entity
  */
-class RepeatEvent extends Event
+class RepeatEvent extends BaseEntity
 {
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="startdate", type="datetimetz")
+     */
+    protected $nextdate;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="duration", type="integer", nullable=true)
+     */
+    protected $duration;
+
     /**
      * @var string
      *
      * @ORM\Column(name="name", type="string", length=255)
      */
-    private $repeat_pattern = '';
+    protected $repeat_pattern = '';
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="summary", type="string", length=255)
+     */
+    protected $summary;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="text", nullable=true)
+     */
+    protected $description;
+
+    /**
+     * @var Location
+     *
+     * @ORM\ManyToOne(targetEntity="Location")
+     * @ORM\JoinColumn(name="locations_id", referencedColumnName="id")
+     */
+    protected $location;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="url", type="string", length=255, nullable=true)
+     */
+    protected $url;
+
+    /**
+     * @var array
+     *
+     * @ORM\ManyToMany(targetEntity="Tag")
+     * @ORM\JoinTable(name="repeat_events2tags",
+     *      joinColumns={@ORM\JoinColumn(name="repeat_events_id", referencedColumnName="id")},
+     *      inverseJoinColumns={@ORM\JoinColumn(name="tags_id", referencedColumnName="id")}
+     *      )
+     */
+    protected $tags = [];
 
 }
