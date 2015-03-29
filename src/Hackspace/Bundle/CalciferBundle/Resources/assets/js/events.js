256edf5ad3f35d4dd9b33ba9776668b71ca98e27
Tim Schumacher <tim@bandenkrieg.hacked.jp>
2015-03-29 22:38:51 +0200

    Use SSL-tiles.

--- src/Hackspace/Bundle/CalciferBundle/Resources/assets/js/events.js
+++ src/Hackspace/Bundle/CalciferBundle/Resources/assets/js/events.js
@@ -22,7 +22,7 @@ jQuery(document).ready(function () {
         map = L.map('map');
 
         // add an OpenStreetMap tile layer
-        L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+        L.tileLayer('https://{s}.tile.osm.org/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         }).addTo(map);
 
